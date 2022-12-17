d45180c9544ced15753f0243d6e538a9966aec08
sethdanny <naddulidaniel1994@gmail.com>
2022-12-17 17:25:54 +0300

    linear search

---
+++ locate_cards.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def locate_card(cards=[], query):
+    position = 0
+
+    while True:
+        if cards[position] == query:
+            return position
+        position += 1
+
+        if position == len(cards):
+            return -1
